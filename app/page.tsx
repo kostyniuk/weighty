0e256f7818510dec4057d037a4ce5a0bba6bd01b
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-11-11 22:49:20 +0100

    first version of chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import { CardsBurnedToday } from "@/components/ui/cards/burned-today";
 import { CardsActivityToday } from "@/components/ui/cards/activity-today";
 import { CardsWeightToday } from "@/components/ui/cards/weight-today";
+import { CardsProgress } from "@/components/ui/cards/progress";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black gap-4">
-      <CardsBurnedToday />
-      <CardsActivityToday />
-      <CardsWeightToday />
-    </div>
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black gap-4">
+        <CardsBurnedToday />
+        <CardsActivityToday />
+        <CardsWeightToday />
+      </div>
+      <div>
+        <CardsProgress />
+      </div>
+    </>
   );
 }
